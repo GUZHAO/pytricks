fa89b43ab3fc78721800676083e1b49ffa128f4c
GUZHAO <guzhao1990@gmail.com>
2018-04-03 23:23:41 -0400

    End of Day upload

---
+++ json.dumps().py
@@ -0,0 +1,8 @@
+# The standard string repr for dicts is hard to read;
+import json
+
+my_mapping = {'a': 23, 'b': 42, 'c': 0xc0ffee}
+print(my_mapping)
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
+
+print(json.dumps({all: 'yup'}))
